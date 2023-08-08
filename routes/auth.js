b449a4c69d495f7ba826e697b594ea798d31ae75
arjunjoshua <arjunjoshua@gmail.com>
2023-08-08 13:00:52 +0100

    Changed auth to use email instead of username Updated notification route to put read status

--- routes/auth.js
+++ routes/auth.js
@@ -6,20 +6,21 @@ const User = require('../models/User');
 const router = express.Router();
 
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
   
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ email });
   if (!user) {
-    return res.status(400).json({ message: 'Invalid username or password' });
+    return res.status(400).json({ message: 'Invalid email or password' });
   }
 
 
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
-    return res.status(400).json({ message: 'Invalid username or password' });
+    return res.status(400).json({ message: 'Invalid email or password' });
   }
 
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  const { username } = user;
 
   res.json({ status: 'success', token, username, userId: user._id });
 });
--- routes/notifications.js
+++ routes/notifications.js
@@ -14,4 +14,22 @@ router.get('/', async (req, res) => {
     res.json(user.notifications);
 });
 
+router.put('/', async (req, res) => {
+    const { userID, notificationID } = req.body;
+    const user = await User.findById(userID);
+    if (!user)
+        return res.status(400).json({ status: 'error', message: 'User not found' });
+
+    const selectedNotification = user.notifications.id(notificationID);
+    selectedNotification.isRead = true;
+
+    if (!selectedNotification)
+        return res.status(400).json({ status: 'error', message: 'Notification not found' });
+
+    await user.save();
+    const unreadNotifications = user.notifications.filter(notification => !notification.isRead);
+    res.json(unreadNotifications);
+});
+
+
 module.exports = router;
