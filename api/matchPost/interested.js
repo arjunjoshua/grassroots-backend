28006aa6224f4e46d181b8e68365f7d42078caec
arjunjoshua <arjunjoshua@gmail.com>
2023-08-17 18:28:49 +0100

    Removed the try and catch block in interested.js Added indexing to coach_id in Team.js

--- api/matchPost/interested.js
+++ api/matchPost/interested.js
@@ -8,61 +8,56 @@ module.exports = async (req, res) => {
 
     const {userID, matchID, isInterested, teamID} = req.body;
 
-    try {
-        const selectedMatch = await MatchPost.findById(matchID).populate('coach_id');
-        if (!selectedMatch) {
-            return res.status(400).json({ status: 'error', message: 'Match not found' });
-        }
-
-        const interestedUser = await User.findById(userID);
-        if (!interestedUser){
-            return res.status(400).json({ status: 'error', message: 'User not found' });
-        }
-
-        
-        const interestedTeam = await Team.findById(teamID);
-        if (!interestedTeam) {
-            return res.status(400).json({ status: 'error', message: 'Team not found' });
-        }
-           
-        if (!isInterested) {   
-            const index = selectedMatch.interested_users.indexOf(userID);
-            if (index > -1) {
-                selectedMatch.interested_users.splice(index, 1);
-                selectedMatch.interested_users_names.splice(index, 1);
-                await selectedMatch.save();
-            }
-            return res.json({ status: 'success' });
-        }
-
-       const postingUser = selectedMatch.coach_id;
-       if (!postingUser) {
-            return res.status(400).json({ status: 'error', message: 'Posting user not found' });
-       }
-            
-       if (!postingUser.notifications) {
-              postingUser.notifications = [];
-         }
-        postingUser.notifications.push({
-            category: 'request',
-            message: `${interestedUser.username} is interested in your match post`,
-            isRead: false,
-            date: Date.now(),
-            matchID: selectedMatch._id,
-            interested_team_name: interestedTeam.team_name,
-            sendingUser: userID,
-        });
-             
-        selectedMatch.interested_users_names.push(interestedUser.username);
-        selectedMatch.interested_users.push(userID);
-
-        await postingUser.save();
-        await selectedMatch.save();
-        
-        return res.json({ status: 'success' });
-    } 
-catch (error) {
-        return res.status(500).json({ status: 'error', message: 'Server error' });
+    const selectedMatch = await MatchPost.findById(matchID);
+    if (!selectedMatch) {
+        return res.status(400).json({ status: 'error', message: 'Match not found' });
     }
+
+    const interestedUser = await User.findById(userID);
+    if (!interestedUser){
+        return res.status(400).json({ status: 'error', message: 'User not found' });
+    }
+
+    
+    const interestedTeam = await Team.findById(teamID);
+    if (!interestedTeam) {
+        return res.status(400).json({ status: 'error', message: 'Team not found' });
+    }
+        
+    if (!isInterested) {   
+        const index = selectedMatch.interested_users.indexOf(userID);
+        if (index > -1) {
+            selectedMatch.interested_users.splice(index, 1);
+            selectedMatch.interested_users_names.splice(index, 1);
+            await selectedMatch.save();
+        }
+        return res.json({ status: 'success' });
+    }
+
+    const postingUser = await User.findById(selectedMatch.coach_id);
+    if (!postingUser) {
+        return res.status(400).json({ status: 'error', message: 'Posting user not found' });
+    }
+        
+    if (!postingUser.notifications) {
+            postingUser.notifications = [];
+        }
+    postingUser.notifications.push({
+        category: 'request',
+        message: `${interestedUser.username} is interested in your match post`,
+        isRead: false,
+        date: Date.now(),
+        matchID: selectedMatch._id,
+        interested_team_name: interestedTeam.team_name,
+        sendingUser: userID,
+    });
+            
+    selectedMatch.interested_users_names.push(interestedUser.username);
+    selectedMatch.interested_users.push(userID);
+
+    await postingUser.save();
+    await selectedMatch.save();
+    
+    return res.json({ status: 'success' });    
 }
         
--- database/models/Team.js
+++ database/models/Team.js
@@ -5,7 +5,7 @@ const TeamSchema = new mongoose.Schema({
   age_group: { type: String, required: true },
   proficiency_level: { type: String, required: true },
   kit_color: { type: String, required: true },
-  coach_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  coach_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true },
 });
 
 module.exports = mongoose.model('Team', TeamSchema);
